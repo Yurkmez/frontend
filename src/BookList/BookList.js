8b1d5853814a135d914181e837186e7ad7ce7485
Yurkmez <yurkmez.shaikh@gmail.com>
2024-03-04 16:57:38 +0300

    Added delete functional

--- src/BookList/BookList.js
+++ src/BookList/BookList.js
@@ -1,9 +1,14 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteBook } from '../redux/books/actionCreators';
 import './BookList.css';
 
 const BookList = () => {
     // Здесь мы подписываемся на состояние "books"
     const books = useSelector((state) => state.books);
+    const dispatch = useDispatch();
+    const handleDeleteBook = (id) => {
+        dispatch(deleteBook(id));
+    };
     return (
         <div className="app-block book-list">
             <h2>Book List</h2>
@@ -16,6 +21,13 @@ const BookList = () => {
                             <div className="book-info">
                                 {++i}. {book.title} by{' '}
                                 <strong>{book.author}</strong>
+                            </div>
+                            <div className="book-actions">
+                                <button
+                                    onClick={() => handleDeleteBook(book.id)}
+                                >
+                                    Delete
+                                </button>
                             </div>
                         </li>
                     ))}
